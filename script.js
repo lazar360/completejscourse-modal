e5d260c80856100d0e4d6b6ef4378b8a76d2554d
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-04-11 13:54:49 +0200

    maj script

--- script.js
+++ script.js
@@ -3,10 +3,23 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+const openModal = function () {
+  modal.classList.remove('hidden'); //ne pas utiliser le point
+  overlay.classList.remove('hidden');
+};
 
 for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('Button clicked');
-    modal.classList.remove('hidden'); //ne pas utiliser le point
-  });
+  btnsOpenModal[i].addEventListener('click', openModal); // ne pas utiliser les parenthèses
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
